2f5f74cdee436d8477b7c39ba31e549672926c78
vasco <vasco.schiavo@protonmail.com>
2023-10-24 22:58:47 +0200

    added new subclasses

--- sequencepy/base/sequence.py
+++ sequencepy/base/sequence.py
@@ -1,64 +1,136 @@
-from abc import ABC, abstractmethod, abstractclassmethod
+from abc import ABC, abstractmethod
 from itertools import islice
-from typing import Generator, List, Tuple, Union
+from typing import Generator, List, Tuple
 
 
 class Sequence(ABC):
-
-    def __init__(self):
-        self.formula_type: str = ''
-
-        self.start_terms: Tuple[int] = tuple()
-
-    def __len__(self):
-        raise ValueError('The sequence contains an infinite number of elements')
 
     @abstractmethod
     def __contains__(self, item):
         raise NotImplementedError
 
-    def __iter__(self, item):
-        return self.generator()
+    def __iter__(self):
+        return self.as_generator()
 
     def __getitem__(self, item):
-        return self.get_element(index=item)
+        return self._at(index=item)
 
-    def generator(self, start: int = 0) -> Generator:
-        if self.formula_type == 'explicit':
-            return self._generator_explicit_sequence(start=start)
-        elif self.formula_type == 'recursive':
-            return self._generator_recursive_sequence()
-        elif self.formula_type == 'partial_sum':
-            raise NotImplementedError
-        else:
-            raise NotImplementedError
+    @abstractmethod
+    def is_finite(self) -> bool:
+        raise NotImplementedError
 
-    def _generator_explicit_sequence(self, start: int = 0) -> Generator:
-        index = start
+    @abstractmethod
+    def as_generator(self) -> Generator:
+        raise NotImplementedError
+
+    @abstractmethod
+    def as_list(self, end: int, start: int = 0, step: int = 1) -> List[int]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _at(self, index: int) -> int:
+        raise NotImplementedError
+
+
+class FiniteSequence(Sequence):
+
+    def __init__(self):
+        super().__init__()
+        self.sequence: List[int] = None
+
+    def __contains__(self, item: int) -> bool:
+        return item in self.sequence
+
+    def __len__(self) -> int:
+        return len(self.sequence)
+
+    @property
+    def is_finite(self) -> bool:
+        return True
+
+    def as_generator(self) -> Generator:
+        for element in self.sequence:
+            yield element
+
+    def as_list(self, end: int, start: int = 0, step: int = 1) -> List[int]:
+        return self.sequence[start:end:step]
+
+    def _at(self, index: int) -> int:
+        return self.sequence[index]
+
+
+class InfiniteSequence(Sequence):
+
+    @property
+    def is_finite(self) -> bool:
+        return False
+
+    def __len__(self) -> int:
+        raise ValueError
+
+    def as_list(self, end: int, start: int = 0, step: int = 1) -> List[int]:
+        return list(islice(self, start, end, step))
+
+    def _at(self, index: int) -> int:
+        return next(islice(self, index, index + 1))
+
+
+class MonotonlyIncreasing(Sequence):
+
+    def __contains__(self, item: int) -> bool:
+        for element in self.as_generator():
+            if element == item:
+                return True
+            if element > item:
+                return False
+
+
+class Explicit(InfiniteSequence):
+
+    def __init__(self, start_index: int = 0):
+        super().__init__()
+        self.start_index = start_index
+
+    def as_generator(self) -> Generator:
+        index = self.start_index
         while True:
             yield self.formula(index=index)
             index += 1
 
-    def _generator_recursive_sequence(self) -> Generator:
+    @abstractmethod
+    def formula(self, index: int) -> int:
+        raise NotImplementedError
+
+
+class Recursive(InfiniteSequence):
+
+    def __init__(self, start_terms: Tuple[int, ...] = None):
+        super().__init__()
+        self.start_terms = start_terms
+
+    def as_generator(self) -> Generator:
         terms = self.start_terms
         while True:
             yield terms[0]
             terms = self.formula(terms=terms)
 
-    def _generator_partial_sum_sequence(self) -> Generator:
-        index = 0
-        partial_sum = 0
-        while True:
-            yield partial_sum
-            partial_sum += self.formula(index=index)
-
     @abstractmethod
-    def formula(self, **kwargs) -> Union[Tuple, int]:
+    def formula(self, terms: Tuple[int, ...]) -> Tuple[int, ...]:
         raise NotImplementedError
 
-    def list(self, end: int, start: int = 0) -> List[int]:
-        # FIXME: decide how we want to start
-        return list(islice(self.generator(), start, end))
 
-    def get_element(self, index: int) -> int:
-        return next(islice(self.generator(), index, index + 1))
+class PropertyDefined(InfiniteSequence):
+
+    def __contains__(self, item: int) -> bool:
+        return self.property(number=item)
+
+    def as_generator(self) -> Generator:
+        number = 1
+        while True:
+            if self.property(number=number):
+                yield number
+            number += 1
+
+    @abstractmethod
+    def property(self, number: int) -> bool:
+        raise NotImplementedError
