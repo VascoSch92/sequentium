fa09949e56b41d398502c2bba76f4a6682701b97
vasco <vasco.schiavo@protonmail.com>
2023-11-20 22:20:01 +0100

    small changes in core

--- sequence/core/core.py
+++ sequence/core/core.py
@@ -1,9 +1,9 @@
 from abc import ABC, abstractmethod
 from itertools import islice
-from typing import Generator, List, Tuple, Any
+from typing import Generator, List, Any
 
-from sequence.core.validations import validate_positive_integer, _validate_as_list_input
-from sequence.core.errors import InfiniteSequenceError
+from sequence.core.utils.validations import validate_positive_integer, validate_as_list_input
+from sequence.core.utils.errors import InfiniteSequenceError
 
 
 class Sequence(ABC):
@@ -22,6 +22,14 @@ class Sequence(ABC):
     def is_finite(self) -> bool:
         raise NotImplementedError
 
+    @property
+    def is_periodic(self) -> bool:
+        return False
+
+    @property
+    def period(self) -> int:
+        return 0
+
     @abstractmethod
     def as_generator(self) -> Generator:
         raise NotImplementedError
@@ -35,7 +43,7 @@ class Sequence(ABC):
         raise NotImplementedError
 
 
-class FiniteSequence(Sequence):
+class FiniteType(Sequence, ABC):
 
     def __init__(self):
         super().__init__()
@@ -44,26 +52,12 @@ class FiniteSequence(Sequence):
     def __contains__(self, item: int) -> bool:
         return item in self.sequence
 
-    def __len__(self) -> int:
-        return len(self.sequence)
-
-    @property
-    def is_finite(self) -> bool:
-        return True
-
     def as_generator(self) -> Generator:
         for element in self.sequence:
             yield element
 
-    def as_list(self, end: int, start: int = 0, step: int = 1) -> List[int]:
-        _validate_as_list_input(start=start, end=end, step=step)
-        return self.sequence[start:end:step]
 
-    def _at(self, index: int) -> Any:
-        return self.sequence[index]
-
-
-class InfiniteSequence(Sequence):
+class InfiniteType(Sequence, ABC):
 
     @property
     def is_finite(self) -> bool:
@@ -72,9 +66,9 @@ class InfiniteSequence(Sequence):
     def __len__(self) -> int:
         raise InfiniteSequenceError
 
-    def as_list(self, end: int, start: int = 0, step: int = 1) -> List[int]:
-        _validate_as_list_input(start=start, end=end, step=step)
-        return list(islice(self, start, end, step))
+    def as_list(self, stop: int, start: int = 0, step: int = 1) -> List[int]:
+        validate_as_list_input(start=start, stop=stop, step=step)
+        return list(islice(self, __start=start, __stop=stop, __step=step))
 
     def _at(self, index: int) -> int:
         validate_positive_integer(integer=index)
