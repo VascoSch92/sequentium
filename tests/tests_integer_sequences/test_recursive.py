7f035e8ef641be792bc61a60209f16061cfbf62b
vasco <vasco.schiavo@protonmail.com>
2023-11-24 22:41:44 +0100

    add test A000032

--- tests/tests_integer_sequences/test_recursive.py
+++ tests/tests_integer_sequences/test_recursive.py
@@ -2,6 +2,16 @@ from tests.sequence_test_suite import SequenceTestSuite
 from sequence.sequences.integer.recursive import *
 
 
+class TestA000032(SequenceTestSuite):
+    sequence = A000032()  # Lucas Numbers
+    ground_truth = [
+        2, 1, 3, 4, 7, 11, 18, 29, 47, 76, 123, 199, 322, 521, 843, 1364, 2207, 3571, 5778, 9349, 15127, 24476, 39603,
+        64079, 103682, 167761, 271443, 439204, 710647, 1149851, 1860498, 3010349, 4870847, 7881196, 12752043, 20633239,
+        33385282, 54018521, 87403803,
+    ]
+    ground_truth_length = len(ground_truth)
+
+
 class TestA000045(SequenceTestSuite):
     sequence = A000045()  # Fibonacci numbers
     ground_truth = [
