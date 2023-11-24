dd5f16e9815a35f68fea96de9c71c679afa0a640
vasco <vasco.schiavo@protonmail.com>
2023-11-24 23:19:28 +0100

    rewrite A000040

--- sequence/sequences/integer/property_defined.py
+++ sequence/sequences/integer/property_defined.py
@@ -1,15 +1,23 @@
-from sequence.core.infinite_type import PropertyDefined
-from sequence.core.utils.functions import is_prime
+from typing import Any
+
+from sequence.core.infinite_type import Explicit
+from sequence.sequences.integer.property_defined_generalised_sequences import TypesOfPrimes
 
 
-class A000040(PropertyDefined):
+class IntegerSequance(Explicit):
+
+    def __contains__(self, item):
+        return True
+
+    def formula(self, index: int) -> Any:
+        return index
+
+
+class A000040(TypesOfPrimes):
     """ The prime numbers (https://oeis.org/A000040) """
 
     def __init__(self):
-        super().__init__()
-
-    def property(self, number: int) -> bool:
-        return is_prime(number=number)
+        super().__init__(base_sequence=IntegerSequance)
 
 
 class PrimeNumbers(A000040):
