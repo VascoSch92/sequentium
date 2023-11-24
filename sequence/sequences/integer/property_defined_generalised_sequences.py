d9496ceaedf3ac903bfe735e8883952ff1bf5d69
vasco <vasco.schiavo@protonmail.com>
2023-11-24 23:19:07 +0100

    add TypesOfPrimes class

---
+++ sequence/sequences/integer/property_defined_generalised_sequences.py
@@ -0,0 +1,22 @@
+from typing import Generator
+
+from sequence.core.core import InfiniteType
+from sequence.core.infinite_type import PropertyDefined
+from sequence.core.utils.functions import is_prime
+
+
+class TypesOfPrimes(PropertyDefined):
+
+    def __init__(self, base_sequence: InfiniteType):
+        super().__init__()
+        self.base_sequence = base_sequence()
+
+    def as_generator(self) -> Generator:
+        number = 1
+        while True:
+            if self.property(number=number) and number in self.base_sequence:
+                yield number
+            number += 1
+
+    def property(self, number: int) -> bool:
+        return is_prime(number=number)
