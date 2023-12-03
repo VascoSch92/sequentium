c7b798afdd41c39b202967d94f0ac3cc226d3bbb
vasco <vasco.schiavo@protonmail.com>
2023-12-03 22:35:23 +0100

    add version here

--- sequence/__init__.py
+++ sequence/__init__.py
@@ -4,3 +4,5 @@ from sequence.sequences.integer.periodic import *
 from sequence.sequences.integer.property_defined import *
 from sequence.sequences.integer.recursive import *
 
+__version__ = '0.0.1'
+
