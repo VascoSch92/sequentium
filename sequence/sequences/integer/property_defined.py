62cd7b8d624860c3a0f4b0a9e28da22f4741defe
vasco <vasco.schiavo@protonmail.com>
2023-12-03 16:05:59 +0100

    add A002808 sequence

--- sequence/sequences/integer/property_defined.py
+++ sequence/sequences/integer/property_defined.py
@@ -1,9 +1,11 @@
 from sequence.sequences.integer.explicit import A000027
 from sequence.sequences.integer.property_defined_generalised_sequences import TypesOfPrimes
+from sequence.core.infinite_type import PropertyDefined
+from sequence.core.utils.functions import is_prime
 
 
 class A000040(TypesOfPrimes):
-    """ The prime numbers (https://oeis.org/A000040) """
+    """ The prime numbers (https://oeis.org/A000040)."""
 
     def __init__(self):
         super().__init__(base_sequence=A000027)
@@ -11,3 +13,19 @@ class A000040(TypesOfPrimes):
 
 PrimeNumbers = A000040
 PositivePrimeNumbers = A000040
+
+
+class A002808(PropertyDefined):
+    """The composite numbers (https://oeis.org/A002808)."""
+
+    def __init__(self):
+        super().__init__()
+
+    def property(self, number: int) -> bool:
+        if number <= 3:
+            return False
+        return is_prime(number=number) is False
+
+
+CompositeNumbers = A002808
+PositiveCompositeNumbers = A002808
