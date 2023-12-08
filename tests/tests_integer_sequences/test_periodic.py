5ba95e71d8c51eda995981ccd7a246248741839e
vasco <vasco.schiavo@protonmail.com>
2023-12-08 10:29:11 +0100

    add tests

--- tests/tests_integer_sequences/test_periodic.py
+++ tests/tests_integer_sequences/test_periodic.py
@@ -1,2 +1,18 @@
+from tests.sequence_test_suite import PeriodicSequenceTestSuite
+from sequence.sequences.integer.periodic import *
+
+
+class TestA128834(PeriodicSequenceTestSuite):
+    sequence = A128834()
+    sequence_name = 'sequence A128834'
+    period_length = 6
+    ground_truth = [
+        0, 1, 1, 0, -1, -1, 0, 1, 1, 0, -1, -1, 0, 1, 1, 0, -1, -1, 0, 1, 1, 0, -1, -1, 0, 1, 1, 0, -1, -1, 0, 1, 1,
+        0, -1, -1, 0, 1, 1, 0, -1, -1, 0, 1, 1, 0, -1, -1, 0, 1, 1, 0, -1, -1, 0, 1, 1, 0, -1, -1, 0, 1, 1, 0, -1, -1,
+        0, 1, 1, 0, -1, -1, 0, 1, 1, 0, -1, -1, 0, 1, 1, 0, -1, -1, 0, 1, 1, 0, -1, -1, 0, 1, 1, 0, -1, -1, 0, 1, 1, 0,
+        -1, -1, 0, 1, 1,
+    ]
+    ground_truth_length = len(ground_truth)
+
 
 
