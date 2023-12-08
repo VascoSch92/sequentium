cf6394b43503500137296a24c841ac3186278b28
vasco <vasco.schiavo@protonmail.com>
2023-12-08 10:29:50 +0100

    add A128834

---
+++ sequence/sequences/integer/periodic.py
@@ -0,0 +1,12 @@
+from sequence.core.finite_type import Periodic
+
+
+class A128834(Periodic):
+    """Sequence A128834 (https://oeis.org/A128834)."""
+
+    def __init__(self):
+        super().__init__(_period=6)
+        self.sequence = [0, 1, 1, 0, -1, -1]
+
+    def __str__(self):
+        return 'sequence A128834'
