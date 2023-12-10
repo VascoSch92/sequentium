b4fc342725627cdc2b6300d0fba0bd140e945d60
vasco <vasco.schiavo@protonmail.com>
2023-12-10 13:32:09 +0100

    refactoring

--- sequentium/cli/cli.py
+++ sequentium/cli/cli.py
@@ -1,82 +1,10 @@
 import argparse
 import importlib
 import sys
+import re
+
 from sequentium.cli.parser import CliParser
-from sequentium import __version__
 from sequentium.core.core import Sequence
-from sequentium.core.utils.exceptions import InfiniteSequenceError
-
-
-def parser_command_line_arguments() -> argparse.Namespace:
-    """Parse command line arguments."""
-    parser = argparse.ArgumentParser(
-        prog='Sequentium',
-        description='Sequentium is a tool for working with mathematical sequences.',
-        epilog='For help with a specific command, see: `sequentium help <command>`.',
-    )
-    group = parser.add_mutually_exclusive_group()
-
-    group.add_argument(
-        '-v',
-        '--version',
-        action='version',
-        version=f'{parser.prog}: v{__version__}',
-        help='Display the version information.',
-    )
-
-    group.add_argument(
-        '--list',
-        action='store_true',
-        help="List a list of implemented sequences.",
-    )
-
-    group.add_argument(
-        "sequentium",
-        type=str,
-        nargs="?",
-        help="Specify the name or identifier of the sequentium to operate on.",
-    )
-    parser.add_argument(
-        '-a',
-        '--at',
-        type=int,
-        default=None,
-        help='Retrieve the term at the specified index in the sequentium.',
-    )
-    parser.add_argument(
-        '-l',
-        '--length',
-        action='store_true',
-        default=None,
-        help='Display the length of the sequentium.',
-    )
-    parser.add_argument(
-        "--start",
-        type=int,
-        default=None,
-        help="Define the starting point of the sequentium.",
-    )
-    parser.add_argument(
-        "--stop",
-        type=int,
-        default=None,
-        help="End point of the sequentium (excluded).",
-    )
-    parser.add_argument(
-        '--step',
-        type=int,
-        default=None,
-        help='Step size for iterating through the sequentium.',
-    )
-    parser.add_argument(
-        '-c',
-        '--contains',
-        type=int,
-        default=None,
-        help='Check if the sequentium contains a specific value.',
-    )
-
-    return parser.parse_args()
 
 
 def import_sequence_dynamically(sequence: str) -> Sequence:
@@ -88,42 +16,6 @@ def import_sequence_dynamically(sequence: str) -> Sequence:
         sys.exit(f'Sequence {sequence} is not implemented yet!')
     else:
         return sequence_class()
-
-
-def command_line_interface() -> None:
-    """Parses command-line arguments and performs the requested actions."""
-    args = parser_command_line_arguments()
-
-    if args.list:
-        with open('SEQUENCES_LIST.md', 'r') as file:
-            content = file.read()
-            print(content)
-        sys.exit(1)
-
-    if not args.sequence:
-        sys.exit('no sequentium give.. please see --help')
-    else:
-        sequence = import_sequence_dynamically(sequence=args.sequence)
-        if args.at:
-            if args.at < 0:
-                sys.exit(f'invalid int value for --start: {args.at}. Expected a non-negative integer!')
-            print(sequence[args.at])
-            sys.exit(-1)
-        elif args.start is not None:
-            if args.start < 0:
-                sys.exit(f'invalid int value for --start: {args.start}. Expected a non-negative integer!')
-            print(f'{sequence}: {sequence._as_list(start=args.start, stop=args.stop, step=args.step)}')
-        elif args.contains is not None:
-            print(args.contains in sequence)
-        elif args.length:
-            try:
-                sequence_length = len(sequence)
-            except InfiniteSequenceError:
-                sys.exit(f'{sequence}: the sequentium is infinite')
-            else:
-                print(f'{sequence} has order {sequence_length}')
-        else:
-            print(sequence)
 
 
 class CommandLineInterface:
@@ -139,12 +31,19 @@ class CommandLineInterface:
     def print_list(self) -> None:
         with open('SEQUENCES_LIST.md', 'r') as file:
             content = file.read()
-            print(content)
+
+            # Define a regular expression to match the table content
+            table_pattern = re.compile(r'\|.*\|', re.DOTALL)
+            matches = table_pattern.findall(content)
+
+            print('Integer Sequences'.center(200, '-'))
+            print(matches[0])
+
         sys.exit(1)
 
     def execute_sequence_command(self, args: argparse.Namespace) -> None:
         if not args.sequence:
-            sys.exit('no sequentium given.. please see --help')
+            sys.exit('no sequence given.. please see --help')
         else:
             sequence = import_sequence_dynamically(sequence=args.sequence)
             if args.at:
@@ -153,12 +52,17 @@ class CommandLineInterface:
                 self.execute_length_command(sequence)
             elif args.contains:
                 self.execute_contains_command(args, sequence)
-            elif args.start is not None:
-                if args.start < 0:
-                    sys.exit(f'invalid int value for --start: {args.start}. Expected a non-negative integer!')
-                print(f'{sequence}: {sequence._as_list(start=args.start, stop=args.stop, step=args.step)}')
+            elif args.stop is not None:
+                self.execute_as_list_command(args, sequence)
             else:
                 print(sequence.__str__().capitalize())
+
+    def execute_as_list_command(self, args, sequence):
+        if args.start < 0:
+            sys.exit(f'invalid int value for --start: {args.start}. Expected a non-negative integer!')
+        if args.start > args.stop:
+            sys.exit(f'invalid int value for --stop: {args.stop}. Expected a value bigger than --start')
+        print(f'{sequence}: {sequence[args.start:args.stop:args.step]}')
 
     def execute_contains_command(self, args, sequence) -> None:
         sys.exit(args.contains in sequence)
