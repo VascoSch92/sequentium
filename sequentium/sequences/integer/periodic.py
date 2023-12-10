cd1a1fc427e789c54cec5ce9280d04b7aec976b9
vasco <vasco.schiavo@protonmail.com>
2023-12-10 23:04:42 +0100

    add A087204

--- sequentium/sequences/integer/periodic.py
+++ sequentium/sequences/integer/periodic.py
@@ -1,6 +1,18 @@
 from sequentium.core.finite_type import Periodic
 
 
+class A087204(Periodic):
+    """Sequence A087204 (https://oeis.org/A087204)."""
+
+   # TODO: why we cannot put sequence in the constructor?
+    def __init__(self):
+        super().__init__(_period=6)
+        self.sequence = [2, 1, -1, -2, -1, 1]
+
+    def __str__(self):
+        return 'sequence A087204'
+
+
 class A128834(Periodic):
     """Sequence A128834 (https://oeis.org/A128834)."""
 
