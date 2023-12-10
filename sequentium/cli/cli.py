3e30382ea8a3cbccfea8f9efc22ef67d898879db
vasco <vasco.schiavo@protonmail.com>
2023-12-10 23:19:09 +0100

    add docstring

--- sequentium/cli/cli.py
+++ sequentium/cli/cli.py
@@ -8,7 +8,7 @@ from sequentium.core.core import Sequence
 
 
 def import_sequence_dynamically(sequence: str) -> Sequence:
-    """Dynamically import a sequentium class by name."""
+    """Dynamically import a sequence class by name."""
     try:
         sequence_module = importlib.import_module('sequentium')
         sequence_class = getattr(sequence_module, f'{sequence}')
@@ -19,29 +19,49 @@ def import_sequence_dynamically(sequence: str) -> Sequence:
 
 
 class CommandLineInterface:
-
+    """A command-line interface for interacting with sequences."""
     def execute(self) -> None:
+        """
+        Parses command-line arguments using CliParser and executes either the print_list or execute_sequence_command
+        based on the arguments.
+        """
         args = CliParser().parser()
 
         if args.list:
-            self.print_list()
+            self.print_list(args=args)
         else:
             self.execute_sequence_command(args=args)
 
-    def print_list(self) -> None:
+    def print_list(self, args: argparse.Namespace) -> None:
+        """Prints a list of implemented sequences based on the provided argument."""
         with open('SEQUENCES_LIST.md', 'r') as file:
             content = file.read()
 
-            # Define a regular expression to match the table content
-            table_pattern = re.compile(r'\|.*\|', re.DOTALL)
-            matches = table_pattern.findall(content)
-
-            print('Integer Sequences'.center(200, '-'))
-            print(matches[0])
+            if args.list == 'integer':
+                # extract the table integer sequences from the md
+                integer_sequences_pattern = re.compile(
+                    r'## Integer sequences\n(.+?)\n## Generalised sequences',
+                    re.DOTALL,
+                )
+                match = re.search(integer_sequences_pattern, content)
+                generalised_sequences_table = match.group(1).strip()
+                print(generalised_sequences_table)
+            elif args.list == 'generalised':
+                # extract the table generalised sequences from the md
+                generalised_sequences_pattern = re.compile(r'## Generalised sequences\n(.+?)(?=\n##|$)', re.DOTALL)
+                match = re.search(generalised_sequences_pattern, content)
+                generalised_sequences_table = match.group(1).strip()
+                print(generalised_sequences_table)
+            else:
+                # Define a regular expression to match the table content
+                table_pattern = re.compile(r'\|.*\|', re.DOTALL)
+                tables = table_pattern.findall(content)[0]
+                print(tables)
 
         sys.exit(1)
 
     def execute_sequence_command(self, args: argparse.Namespace) -> None:
+        """Executes a specific sequence command based on the provided arguments."""
         if not args.sequence:
             sys.exit('no sequence given.. please see --help')
         else:
@@ -50,7 +70,7 @@ class CommandLineInterface:
                 self.execute_at_command(args, sequence)
             elif args.length:
                 self.execute_length_command(sequence)
-            elif args.contains:
+            elif args.c:
                 self.execute_contains_command(args, sequence)
             elif args.stop is not None:
                 self.execute_as_list_command(args, sequence)
@@ -65,7 +85,7 @@ class CommandLineInterface:
         print(f'{sequence}: {sequence[args.start:args.stop:args.step]}')
 
     def execute_contains_command(self, args, sequence) -> None:
-        sys.exit(args.contains in sequence)
+        sys.exit(args.c in sequence)
 
     def execute_length_command(self, sequence) -> None:
         if sequence.is_finite:
--- sequentium/cli/parser.py
+++ sequentium/cli/parser.py
@@ -3,14 +3,17 @@ from sequentium import __version__
 
 
 class CliParser:
+    """ A command-line interface parser"""
 
     def parser(self) -> argparse.Namespace:
+        """Parses command-line arguments and returns the parsed arguments as a namespace."""
         parser = self.create_parser()
         parser = self.add_groups_to_parser(parser=parser)
-        parser = self.add_arguments(parser=parser)
+        parser = self.add_sequence_arguments(parser=parser)
         return parser.parse_args()
 
     def create_parser(self) -> argparse.ArgumentParser:
+        """Creates an ArgumentParser with program name, description, and epilog."""
         return argparse.ArgumentParser(
             prog='Sequentium',
             description='Sequentium is a tool for working with mathematical sequences.',
@@ -18,6 +21,10 @@ class CliParser:
         )
 
     def add_groups_to_parser(self, parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
+        """
+        Adds mutually exclusive argument groups for version information, listing sequences,
+        and sequence identification.
+        """
         group = parser.add_mutually_exclusive_group()
         group.add_argument(
             '-v',
@@ -28,55 +35,60 @@ class CliParser:
         )
         group.add_argument(
             '--list',
-            action='store_true',
+            nargs='?',
+            choices=['all', 'integer', 'generalised'],
             help="List a list of implemented sequences.",
         )
         group.add_argument(
-            "sequentium",
+            'sequence',
             type=str,
-            nargs="?",
-            help="Specify the name or identifier of the sequentium to operate on.",
+            nargs='?',
+            help='Specify the name or identifier of the sequence to operate on.',
         )
         return parser
 
-    def add_arguments(self, parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
+    def add_sequence_arguments(self, parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
+        """
+        Adds arguments related to sequence operations such as retrieving terms, displaying length,
+        defining start/stop points, specifying step size, and checking for a specific value.
+        """
         sequence_group = parser.add_argument_group('sequentium options')
         sequence_group.add_argument(
             '-a',
             '--at',
             type=int,
             default=None,
-            help='Retrieve the term at the specified index in the sequentium.',
+            help='Retrieve the term at the specified index in the sequence.',
         )
         sequence_group.add_argument(
             '-l',
             '--length',
             action='store_true',
             default=None,
-            help='Display the length of the sequentium.',
+            help='Display the length of the sequence.',
         )
         sequence_group.add_argument(
             "--start",
             type=int,
             default=None,
-            help="Define the starting point of the sequentium.",
+            help="Define the starting point of the sequence.",
         )
         sequence_group.add_argument(
             "--stop",
             type=int,
             default=None,
-            help="End point of the sequentium (excluded).",
+            help="End point of the sequence (excluded).",
         )
         sequence_group.add_argument(
             '--step',
             type=int,
             default=None,
-            help='Step size for iterating through the sequentium.',
+            help='Step size for iterating through the sequence.',
         )
         sequence_group.add_argument(
             '-c',
             type=int,
             default=None,
-            help='Check if the sequentium contains a specific value.',
+            help='Check if the sequence contains a specific value.',
         )
         return parser
