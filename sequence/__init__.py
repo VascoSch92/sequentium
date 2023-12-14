4d3b428cd6b196395a13e4af3f783df897c87b3d
vasco <vasco.schiavo@protonmail.com>
2023-12-14 07:11:17 +0100

    moved

---
+++ sequence/__init__.py
@@ -0,0 +1,8 @@
+from sequence.sequences.integer.explicit import *
+from sequence.sequences.integer.finite import *
+from sequence.sequences.integer.periodic import *
+from sequence.sequences.integer.property_defined import *
+from sequence.sequences.integer.recursive import *
+
+__version__ = '0.0.1'
+
