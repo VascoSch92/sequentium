6db576f1d9430904f5c040008c73f349f3037ed3
vasco <vasco.schiavo@protonmail.com>
2023-12-14 07:11:37 +0100

    removed

--- sequentium/__init__.py
+++
@@ -1,8 +0,0 @@
-from sequentium.sequences.integer.explicit import *
-from sequentium.sequences.integer.finite import *
-from sequentium.sequences.integer.periodic import *
-from sequentium.sequences.integer.property_defined import *
-from sequentium.sequences.integer.recursive import *
-
-__version__ = '0.0.1'
-
