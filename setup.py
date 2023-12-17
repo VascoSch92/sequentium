b5698ed98ba740071637fec3c97ee5efa656fc67
vasco <vasco.schiavo@protonmail.com>
2023-12-17 22:38:08 +0100

    update

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
-from sequence import __version__
+from setuptools import setup, find_namespace_packages
 
-from setuptools import setup, find_packages, find_namespace_packages
+from sequence.__version__ import __version__
+
+with open('README.md') as f:
+  long_description = f.read()
 
 setup(
   name='sequentium',
@@ -8,22 +11,29 @@ setup(
   license='MIT',
   author='Vasco Schiavo',
   author_email='vasco.schiavo@protonmail.com',
-  url='https://github.com/VascoSch92',
+  url='https://github.com/VascoSch92/sequentium',
   description='A package to work with sequences',
-  packages=find_namespace_packages(exclude=['tests']),
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  packages=find_namespace_packages(include=['sequence*'], exclude=['tests']),
   entry_points={
         'console_scripts': ['sequence=sequence:main']
     },
-  download_url='https://github.com/user/reponame/archive/v_01.tar.gz',
-  keywords=['MATH'],   # Keywords that define your package best
+  # download_url='https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords=['math', 'mathematics', 'sequence', 'fibonacci'],
   classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Users',
-    'Topic :: Software Development :: Build Tools',
+    'Development Status :: 1 - Planning',
+    'Intended Audience :: Education',
+    'Intended Audience :: End Users/Desktop',
+    'Intended Audience :: Science/Research',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
     'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
+    'Topic :: Education',
+    'Topic :: Scientific/Engineering :: Mathematics',
   ],
+  python_requires ='>=3.9',
 )
 
