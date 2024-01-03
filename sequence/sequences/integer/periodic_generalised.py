6a1722c99b4f28592322c2dfa2b8a0e46b6190fc
vasco <vasco.schiavo@protonmail.com>
2024-01-03 23:26:24 +0100

    add docstring to ConstantSequence class

--- sequence/sequences/integer/periodic_generalised.py
+++ sequence/sequences/integer/periodic_generalised.py
@@ -3,6 +3,12 @@ from sequence.core.utils.validation import validate_positive_integer
 
 
 class ConstantSequence(Periodic):
+    """
+    The class represent a constant sequence of a given value
+
+    Attributes:
+        value (int): the values that the constant sequence is taking
+    """
     sequence_name = 'value sequence'
 
     def __init__(self, value: int) -> None:
