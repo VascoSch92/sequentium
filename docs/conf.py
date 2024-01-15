c7c4dc9aaf1de142dd87814ab304c9bdc8823681
vasco <vasco.schiavo@protonmail.com>
2024-01-15 22:42:18 +0100

    readthedocs

---
+++ docs/conf.py
@@ -0,0 +1,8 @@
+extensions = [
+    "myst_nb",
+    "autoapi.extension",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+]
+autoapi_dirs = ["../sequence"]  # location to parse for API reference
+html_theme = "sphinx_rtd_theme"
